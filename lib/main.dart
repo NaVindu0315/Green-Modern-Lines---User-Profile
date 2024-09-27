ef207bd4559b4982656d81496ecbe9ac9df6172f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-09-27 21:26:41 +0530

    main method chnged

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,21 @@ import 'package:flutter/material.dart';
 import 'Views/ProfileView.dart';
 
 void main() {
-  runApp(const Profile_View());
+  runApp(const MYApp());
+}
+
+class MYApp extends StatefulWidget {
+  const MYApp({Key? key}) : super(key: key);
+
+  @override
+  State<MYApp> createState() => _MYAppState();
+}
+
+class _MYAppState extends State<MYApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Profile_View(),
+    );
+  }
 }
